9348df83805dac9ade4871959ce170e21c0b392b
James Puellmann <james.puellmann@gmail.com>
2017-01-25 09:52:14 -0500

    New git segments and personal theme

--- segments/hostname.py
+++ segments/hostname.py
@@ -16,7 +16,7 @@ def add_hostname_segment():
             host_prompt = ' %m '
         else:
             import socket
-            host_prompt = ' %s ' % socket.gethostname().split('.')[0]
+            host_prompt = ' %s' % socket.gethostname().split('.')[0]
 
         powerline.append(host_prompt, Color.HOSTNAME_FG, Color.HOSTNAME_BG)
 
