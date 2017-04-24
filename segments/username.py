b9838882fd1ead01ca54fc6482a9b7b41033c960
James Puellmann <james.puellmann@gmail.com>
2017-04-24 11:10:17 -0400

    customizations various and sundry

--- segments/username.py
+++ segments/username.py
@@ -2,11 +2,11 @@
 def add_username_segment(powerline):
     import os
     if powerline.args.shell == 'bash':
-        user_prompt = ' \\u '
+        user_prompt = '\\u@'
     elif powerline.args.shell == 'zsh':
         user_prompt = ' %n '
     else:
-        user_prompt = ' %s ' % os.getenv('USER')
+        user_prompt = '%s' % os.getenv('USER')
 
     if os.getenv('USER') == 'root':
         bgcolor = Color.USERNAME_ROOT_BG
